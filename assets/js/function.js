fd1f4dd92228aaf6a6143b6e2097c48ebc218a7c
uttam-mahat <45259009+uttam-mahat@users.noreply.github.com>
2021-02-09 13:29:30 +0545

    print updated

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,4 +1,5 @@
 $(document).ready(function (e) {
+  
   // Add the following code if you want the name of the file appear on select
   $(".custom-file-input").on("change", function () {
     var fileName = $(this).val().split("\\").pop();
